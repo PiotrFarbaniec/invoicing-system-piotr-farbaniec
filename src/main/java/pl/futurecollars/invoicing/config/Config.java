44532c204174cdf2825c67acc4cbaad2a85c9af5
Piotr Farbaniec <piotr.farbaniec7@gmail.com>
2024-04-02 22:05:29 +0200

    new project structure with spring framework

---
+++ src/main/java/pl/futurecollars/invoicing/config/Config.java
@@ -0,0 +1,4 @@
+package pl.futurecollars.invoicing.config;
+
+public class Config {
+}
---
+++ src/main/java/pl/futurecollars/invoicing/controller/InvoiceController.java
@@ -0,0 +1,11 @@
+package pl.futurecollars.invoicing.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceController {
+  public static void main(String[] args) {
+    SpringApplication.run(InvoiceController.class, args);
+  }
+}
