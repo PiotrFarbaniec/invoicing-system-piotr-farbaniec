7e09dbe5bac5eac8a28c1057b65e56c879999459
Piotr Farbaniec <piotr.farbaniec7@gmail.com>
2024-04-13 21:58:49 +0200

    Loggers added to CRUD methods

--- src/main/java/pl/futurecollars/invoicing/db/memory/InMemoryDatabase.java
+++ src/main/java/pl/futurecollars/invoicing/db/memory/InMemoryDatabase.java
@@ -5,9 +5,11 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import lombok.extern.slf4j.Slf4j;
 import pl.futurecollars.invoicing.db.Database;
 import pl.futurecollars.invoicing.model.Invoice;
 
+@Slf4j
 public class InMemoryDatabase implements Database {
 
   private final Map<Integer, Invoice> invoices = new HashMap<>();
@@ -17,6 +19,7 @@ public class InMemoryDatabase implements Database {
   public int save(Invoice invoice) {
     invoice.setId(nextId);
     invoices.put(nextId, invoice);
+    log.info("Invoice: {} successful saved in database", invoice);
     return nextId++;
   }
 
@@ -30,14 +33,18 @@ public class InMemoryDatabase implements Database {
     if (invoices.containsKey(id)) {
       updateInvoice.setId(id);
       invoices.put(id, updateInvoice);
+      log.info("Invoice with id: {} successful updated", id);
     }
+    log.info("No invoice with the specified id: {} in database", id);
   }
 
   @Override
   public void delete(int id) {
     if (invoices.containsKey(id)) {
       invoices.remove(id);
+      log.info("Invoice with id: {} successful deleted", id);
     }
+    log.info("No invoice with the specified id: {} in database", id);
   }
 
   @Override
